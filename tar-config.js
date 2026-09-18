2ce41fe7e4b86fe6c30f610865fec17f7842dc34
savantileone <donovanjspann@gmail.com>
2026-09-18 16:32:20 -0500

    Create tar-config.js

---
+++ tar-config.js
@@ -0,0 +1,3 @@
+// The one place to paste your Google Apps Script "Web app" URL (it ends in /exec).
+// Both the public site and the editor page read it from here.
+window.TAR_API = 'https://script.google.com/macros/s/AKfycbyz478vfIqNYmG6WzJZjSCenveQsO0Uiqv4crK--gpkN5K1lw6vqmcq5bVQAG85cOXzbA/exec';
